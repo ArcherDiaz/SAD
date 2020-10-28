86059073bfbaad72c8e8f48b801fa152f724ced0
ArcherDiaz <gfinderprod@gmail.com>
2020-10-28 08:57:37 -0400

    Added web storage functions

--- sad_lib/lib/StorageClass.dart
+++ sad_lib/lib/StorageClass.dart
@@ -2,10 +2,9 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
+import 'dart:html' as html;
 
 class StorageClass {
-
-  //TODO need to do storage functions for web dev
 
   Future<Map<String, dynamic>> readFromMap(String filename){
     Map<String, dynamic> data = Map();
@@ -182,7 +181,6 @@ class StorageClass {
   }
 
 
-
   Future<void> saveImage(String filename, Uint8List bytes){
     return getApplicationDocumentsDirectory().then((xDirectory) {
       return File("${xDirectory.path}/images/$filename").create(recursive: true).then((file) {
@@ -196,6 +194,20 @@ class StorageClass {
     return getApplicationDocumentsDirectory().then((xDirectory) {
       return File("${xDirectory.path}/images/$filename").readAsBytes();
     });
+  }
+
+
+  ///WEB FUNCTIONS
+  Map<String, dynamic> readFromMapWeb(String key){
+    Map<String, dynamic> data = json.decode(html.window.localStorage["disha"]);
+    return data[key];
+  }
+
+  Future<Map<String, dynamic>> writeToMapWeb(String key, Map<String, dynamic> data){
+    Map<String, dynamic> data = json.decode(html.window.localStorage["disha"]);
+    data.update(key, (value) => data, ifAbsent: ()=> data);
+    html.window.localStorage["disha"] = json.encode(data[key]);
+    return data[key];
   }
 
 }
