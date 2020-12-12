f3039becc60e5a3a26f4214dc06be38734187d66
= <=>
2020-12-11 19:33:43 -0500

    Edited CustomDialog

--- sad_lib/lib/DialogClass.dart
+++ sad_lib/lib/DialogClass.dart
@@ -715,91 +715,33 @@ class DialogClass {
 }
 
 
-const EdgeInsets _defaultInsetPadding = EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0);
 
 class CustomDialog extends StatelessWidget {
-  /// Creates a dialog.
-  ///
-  /// Typically used in conjunction with [showDialog].
+
   const CustomDialog({
     Key key,
     this.backgroundColor,
     this.elevation,
     this.insetAnimationDuration = const Duration(milliseconds: 100),
     this.insetAnimationCurve = Curves.decelerate,
-    this.insetPadding = _defaultInsetPadding,
     this.clipBehavior = Clip.none,
     this.shape,
     this.child,
   }) : assert(clipBehavior != null),
         super(key: key);
 
-  /// {@template flutter.material.dialog.backgroundColor}
-  /// The background color of the surface of this [Dialog].
-  ///
-  /// This sets the [Material.color] on this [Dialog]'s [Material].
-  ///
-  /// If `null`, [ThemeData.cardColor] is used.
-  /// {@endtemplate}
   final Color backgroundColor;
 
-  /// {@template flutter.material.dialog.elevation}
-  /// The z-coordinate of this [Dialog].
-  ///
-  /// If null then [DialogTheme.elevation] is used, and if that's null then the
-  /// dialog's elevation is 24.0.
-  /// {@endtemplate}
-  /// {@macro flutter.material.material.elevation}
   final double elevation;
 
-  /// {@template flutter.material.dialog.insetAnimationDuration}
-  /// The duration of the animation to show when the system keyboard intrudes
-  /// into the space that the dialog is placed in.
-  ///
-  /// Defaults to 100 milliseconds.
-  /// {@endtemplate}
   final Duration insetAnimationDuration;
 
-  /// {@template flutter.material.dialog.insetAnimationCurve}
-  /// The curve to use for the animation shown when the system keyboard intrudes
-  /// into the space that the dialog is placed in.
-  ///
-  /// Defaults to [Curves.decelerate].
-  /// {@endtemplate}
   final Curve insetAnimationCurve;
 
-  /// {@template flutter.material.dialog.insetPadding}
-  /// The amount of padding added to [MediaQueryData.viewInsets] on the outside
-  /// of the dialog. This defines the minimum space between the screen's edges
-  /// and the dialog.
-  ///
-  /// Defaults to `EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0)`.
-  /// {@endtemplate}
-  final EdgeInsets insetPadding;
-
-  /// {@template flutter.material.dialog.clipBehavior}
-  /// Controls how the contents of the dialog are clipped (or not) to the given
-  /// [shape].
-  ///
-  /// See the enum [Clip] for details of all possible options and their common
-  /// use cases.
-  ///
-  /// Defaults to [Clip.none], and must not be null.
-  /// {@endtemplate}
   final Clip clipBehavior;
 
-  /// {@template flutter.material.dialog.shape}
-  /// The shape of this dialog's border.
-  ///
-  /// Defines the dialog's [Material.shape].
-  ///
-  /// The default shape is a [RoundedRectangleBorder] with a radius of 4.0
-  /// {@endtemplate}
   final ShapeBorder shape;
 
-  /// The widget below this widget in the tree.
-  ///
-  /// {@macro flutter.widgets.child}
   final Widget child;
 
   static const RoundedRectangleBorder _defaultDialogShape =
@@ -809,8 +751,8 @@ class CustomDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    final bool isDesktop = size.width > 700 ? true : false;
     final DialogTheme dialogTheme = DialogTheme.of(context);
+    final EdgeInsets insetPadding = (size.width > 700) ? EdgeInsets.symmetric(horizontal: size.width/4, vertical: size.height/12) : EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0);
     final EdgeInsets effectivePadding = MediaQuery.of(context).viewInsets + (insetPadding ?? const EdgeInsets.all(0.0));
     return AnimatedPadding(
       padding: effectivePadding,
